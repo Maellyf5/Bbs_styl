94ca23098232791b5da59941e5e1bae440588e2b
yoana <koko-yoana@hotmail.es>
2020-08-04 14:12:33 +0200

    navbar

--- Bbs_Styl/Bbs_Styl/settings.py
+++ Bbs_Styl/Bbs_Styl/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Bbs_Styl.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,6 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+
             ],
         },
     },
